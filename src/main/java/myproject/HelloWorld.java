1a07337ced06d512a3d5634621672ccbd4a963fe
samsung <samsung@211.176.234.166>
2021-05-22 15:00:46 +0900

    initial commit

---
+++ src/main/java/myproject/HelloWorld.java
@@ -0,0 +1,11 @@
+package myproject;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕하세요.");
+		System.out.println("나는 이번에 반드시 취업을 한다.");
+	}
+
+}
